34ffc6348bcd7e6980c98a52a9ca4c7c34b09dd6
teceo2040-max <teceo2040@gmail.com>
2025-09-10 09:08:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+import streamlit as st
 
-ruta = "https://github.com/VMjavier/claseAnalisisDatos/raw/refs/heads/main/datos_reales.zip"
-df = pd.read_csv(ruta)
-n = 10 # Número de variables más comunes a mostrar
-columna = "Crime" # Cambia esto por la columna que desees analizar
+url = ' https://github.com/teceo2040-max/PROYECTOS-ARGOS/raw/refs/heads/main/datos_generales_ficticios.csv' 
+df = pd.read_csv(url, sep=';', encoding='utf-8')
 
+st.title('Datos Generales Ficticios')
+st.dataframe(df)
 
+seleccion_columnas = ['FECHA_HECHOS', 'DELITO', 'ETAPA', 'FISCAL_ASIGNADO', 'DEPARTAMENTO','MUNICIPIO_HECHOS']
+df = df[seleccion_columnas].sort_values(by='FECHA_HECHOS', ascending=True).reset_index(drop=True)
 
-topN = df[columna].value_counts().nlargest(n).index
-plt.figure(figsize=(10,6))
-sns.countplot(data=df[df[columna].isin(topN)], 
-              x=columna, 
-              order=topN, 
-              palette="tab10")
+df['FECHA_HECHOS'] = pd.to_datetime(df['FECHA_HECHOS'], errors='coerce')
 
-plt.title("Top n tipos de crimen", fontsize=14)#<-- Cambia el título según la columna
-plt.xlabel("Tipo de crimen") #<-- Cambia la etiqueta según la columna
-plt.ylabel("Número de casos") #<-- Cambia lsa etiqueta según la columna
-plt.xticks(rotation=30)
-plt.tight_layout()
-plt.savefig("img/graficoHurto.png", dpi=300, bbox_inches="tight")
-plt.show()
+df_serie_tiempo = df.copy()
+df_serie_tiempo['FECHA_HECHOS'] = df['FECHA_HECHOS'].dt.date
+
+# CONSTRUIR LA PAGINA
+
+st.set_page_config(page_title="Dasboard de Delitos - Fiscalia", layout="centered")
+st.header("Dasborard de Delitos - Fiscalia")
+st.dataframe(df)
+
+#st.markdown(f"<h3>Municipios con más delitos: {max_municipio}</h3>", unsafe_allow_html=True)
+#st.markdown(f"<h3>Cantidad de Delitos: {max_cantidad_delitos}</h3>", unsafe_allow_html=True)
+
+st.subheader("Tipo de Delito")
+delitos = df['DELITO'].value_counts()
+st.bar_chart(delitos)
